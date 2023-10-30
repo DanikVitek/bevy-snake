88db33602752a7d53f61730d165301b2fe7f62ea
Danik Vitek <x3665107@gmail.com>
2023-10-30 11:17:58 +0200

    optimize `snake_move`

--- src/main.rs
+++ src/main.rs
@@ -26,12 +26,12 @@ fn main() {
         .run();
 }
 
-#[derive(Debug, Clone, Copy, Default, States, PartialEq, Eq, Hash)]
-enum AppState {
-    #[default]
-    LoadHandles,
-    Setup,
-}
+// #[derive(Debug, Clone, Copy, Default, States, PartialEq, Eq, Hash)]
+// enum AppState {
+//     #[default]
+//     LoadHandles,
+//     Setup,
+// }
 
 fn setup(mut commands: Commands) {
     commands.spawn(Camera2dBundle {
@@ -51,37 +51,36 @@ fn setup_snake(mut commands: Commands, asset_server: Res<AssetServer>) {
             SpatialBundle::default(),
         ))
         .with_children(|snake| {
+            let head_id = snake
+                .spawn((
+                    SnakeHead,
+                    Direction::default(),
+                    make_sprite(asset_server.load("sprites/snake-head.png"), Vec2::ZERO),
+                ))
+                .id();
             snake.spawn((
-                SnakeHead,
+                TailPiece,
                 Direction::default(),
-                make_sprite(asset_server.load("sprites/snake-head.png"), Vec2::ZERO),
+                PrevId(head_id),
+                make_sprite(
+                    asset_server.load("sprites/snake-end.png"),
+                    Vec2 { x: 0.0, y: -1.0 },
+                ),
             ));
-            snake
-                .spawn((SnakeTail, SpatialBundle::default()))
-                .with_children(|tail| {
-                    tail.spawn((
-                        TailPiece,
-                        Direction::default(),
-                        make_sprite(
-                            asset_server.load("sprites/snake-end.png"),
-                            Vec2 { x: 0.0, y: -1.0 },
-                        ),
-                    ));
-                });
         });
 }
 
-/// Make a sprite bundle at a given position, multiplied by `32.0`
+/// Make a sprite bundle at a given position, multiplied by `CELL_SIZE`
 /// and the sprite anchor is set to [`Anchor::TopLeft`]
 fn make_sprite(texture: Handle<Image>, _position @ Vec2 { x, y }: Vec2) -> SpriteBundle {
     SpriteBundle {
         sprite: Sprite {
-            custom_size: Some(Vec2::new(32.0, 32.0)),
+            custom_size: Some(Vec2::new(CELL_SIZE, CELL_SIZE)),
             anchor: Anchor::TopLeft,
             ..Default::default()
         },
         visibility: Visibility::Visible,
-        transform: Transform::from_xyz(x * 32.0, y * 32.0, 0.0),
+        transform: Transform::from_xyz(x * CELL_SIZE, y * CELL_SIZE, 0.0),
         texture,
         ..Default::default()
     }
@@ -90,28 +89,34 @@ fn make_sprite(texture: Handle<Image>, _position @ Vec2 { x, y }: Vec2) -> Sprit
 fn move_snake(
     time: Res<Time>,
     mut snake: Query<&mut Snake>,
-    mut head: Query<(&Direction, &mut Transform), (With<SnakeHead>, Without<TailPiece>)>,
-    mut tail: Query<(&mut Direction, &mut Transform), (With<TailPiece>, Without<SnakeHead>)>,
+    mut pieces: Query<(&Direction, &mut Transform)>,
 ) {
     let mut snake = snake.get_single_mut().unwrap();
-    if snake.next_move.tick(time.delta()).just_finished() {
-        let (head_direction, mut transform) = head.get_single_mut().unwrap();
-        match *head_direction {
+    if !snake.next_move.tick(time.delta()).just_finished() {
+        return;
+    }
+    drop(snake);
+
+    for (tail_direction, mut transform) in pieces.iter_mut() {
+        match *tail_direction {
             Direction::Left => transform.translation.x -= CELL_SIZE,
             Direction::Right => transform.translation.x += CELL_SIZE,
             Direction::Up => transform.translation.y += CELL_SIZE,
             Direction::Down => transform.translation.y -= CELL_SIZE,
         }
-        for (mut tail_direction, mut transform) in tail.iter_mut() {
-            match *tail_direction {
-                Direction::Left => transform.translation.x -= CELL_SIZE,
-                Direction::Right => transform.translation.x += CELL_SIZE,
-                Direction::Up => transform.translation.y += CELL_SIZE,
-                Direction::Down => transform.translation.y -= CELL_SIZE,
-            }
-        }
     }
 }
+
+// fn update_tail_direction(
+//     snake: Query<&Snake>,
+//     tail: Query<(&mut Direction, &PrevId), With<TailPiece>>,
+//     directions: Query<>
+// ) {
+//     let snake = snake.get_single().unwrap();
+//     if !snake.next_move.just_finished() {
+//         return;
+//     }
+// }
 
 fn control_snake(
     keyboard_input: Res<Input<KeyCode>>,
@@ -144,7 +149,10 @@ struct SnakeTail;
 #[derive(Component)]
 struct TailPiece;
 
-#[derive(Default, Component)]
+#[derive(Clone, Copy, Component)]
+struct PrevId(Entity);
+
+#[derive(Clone, Copy, Default, Component)]
 pub enum Direction {
     Left,
     Right,
